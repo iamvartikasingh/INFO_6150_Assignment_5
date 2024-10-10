fbe6fde4f70a828d529b8706a8fb43c236e86a6f
vartika <singh.vart@northeastern.edu>
2024-10-10 17:12:43 -0400

    Improved UIi

--- script1.js
+++ script1.js
@@ -40,3 +40,28 @@ function validateUser() {
 
     return false;
 }
+function checkLoginInput() {
+    const username = document.getElementById('loginUsername').value.trim();
+    const password = document.getElementById('loginPassword').value.trim();
+
+    const usernamePattern = /^[a-zA-Z0-9]+$/;
+    let isValid = true;
+
+    if (!usernamePattern.test(username) || username.length < 2 || username.length > 20) {
+        document.getElementById('loginUsernameError').innerText = "Username must be 2-20 alphanumeric characters.";
+        isValid = false;
+    } else {
+        document.getElementById('loginUsernameError').innerText = "";
+    }
+
+    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$/;
+    if (!passwordPattern.test(password)) {
+        document.getElementById('loginPasswordError').innerText = "Password must be at least 8 characters long, include 1 uppercase, 1 lowercase, 1 number, and 1 special character.";
+        isValid = false;
+    } else {
+        document.getElementById('loginPasswordError').innerText = "";
+    }
+
+    return isValid;
+}
+
